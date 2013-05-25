3f5339eccfa08a5357dc426b9e8e798879e1d48b
chaveiro <nchaveiro@gmail.com>
2013-05-25 17:22:03 +0200

    Changed serial speed to 9600 for compatibility with arduino ide.

--- examples/EmonLibPro_demo/EmonLibPro_demo.ino
+++ examples/EmonLibPro_demo/EmonLibPro_demo.ino
@@ -60,7 +60,8 @@ EmonLibPro  Emon ;
 
 void setup()
 {
-  Serial.begin(250000);
+  //Serial.begin(250000);
+  Serial.begin(9600);
   Serial.println("Ready");
 
   Emon = EmonLibPro();    // Required.  
