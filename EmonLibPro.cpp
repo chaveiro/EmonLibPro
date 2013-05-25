0399522f5ce3c5702feeb2492e741b8a924f502e
chaveiro <nchaveiro@gmail.com>
2013-05-25 16:10:13 +0200

    Create EmonLibPro.cpp

---
+++ EmonLibPro.cpp
@@ -0,0 +1,415 @@
+/*
+    Energy monitor Library Pro
+    Based on emonTx hardware from OpenEnergyMonitor http://openenergymonitor.org/emon/
+    This is a generic library for any voltage and current sensors.
+    Interrupt based, implements a zero cross detector and phase-locked loop for better precision.
+    Completely line frequency independent.
+    
+    Copyright (C) 2013  Nuno Chaveiro  nchaveiro[at]gmail.com  Lisbon, Portugal
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+extern "C" {
+    #include <inttypes.h>
+    #include <math.h>
+    #include <avr/interrupt.h>
+    #include <avr/io.h>
+}
+
+//#include <Arduino.h>
+#include "EmonLibPro.h"
+
+
+//////////////////////////////////////////////////////////////////////////
+// Global Variables
+//////////////////////////////////////////////////////////////////////////
+
+boolean                     EmonLibPro::FlagCYCLE_FULL;  	// Flags a new cycle.
+boolean                     EmonLibPro::FlagCALC_READY;     // Flags new data ready for calculate
+boolean                     EmonLibPro::FlagINVALID_DATA;   // Flags Invalid data
+uint8_t                     EmonLibPro::pllUnlocked;		// If = 0 pll is locked
+uint8_t                     EmonLibPro::SamplesPerCycle;       // --- Gives number of samples that got summed in summed Cycle Data Structure
+TotVoltageDataStructure     EmonLibPro::CycleV[VOLTSCOUNT];    //  |- Cycle Vars
+TotPowerDataStructure       EmonLibPro::CycleP[CURRENTCOUNT];  // -/
+unsigned long               EmonLibPro::SamplesPerCycleTotal;  // --- Number of cycles added for all sums of Total Var.
+unsigned int                EmonLibPro::CyclesPerTotal;       // --- Number of sums on Total var.
+TotVoltageDataStructure     EmonLibPro::TotalV[VOLTSCOUNT];    //  |- Total Vars (Sum of some cycles)
+TotPowerDataStructure       EmonLibPro::TotalP[CURRENTCOUNT];  // -/
+ResultVoltageDataStructure  EmonLibPro::ResultV[VOLTSCOUNT];   // -\_ Result is here!
+ResultPowerDataStructure    EmonLibPro::ResultP[CURRENTCOUNT]; // -/
+
+
+//////////////////////////////////////////////////////////////////////////
+// ISR routine vars
+//////////////////////////////////////////////////////////////////////////
+uint8_t   		            EmonLibPro::AdcId;              // ADC PIN number of sensor measured
+boolean                     EmonLibPro::FlagPllUpdated;		// Flags that PLL was updated
+boolean                     EmonLibPro::FlagOutOfTime;		// Warn ISR routing did not complete before next timer
+
+SampleStructure             EmonLibPro::Sample[VOLTSCOUNT + CURRENTCOUNT]; //Data for last sample
+
+uint8_t                     EmonLibPro::SamplePerAcc;               // ---Samples counter, Number of samples ajusted/detected by soft pll for each AC cycle
+AccVoltageDataStructure     EmonLibPro::AccumulatorV[VOLTSCOUNT];   //  |- Sum of all samples (copyed to cycle var at end of cycle)
+AccPowerDataStructure       EmonLibPro::AccumulatorP[CURRENTCOUNT]; // -/
+
+signed long                 EmonLibPro::Temp[VOLTSCOUNT + CURRENTCOUNT];     // Internal Aux vars
+
+
+//////////////////////////////////////////////////////////////////////////
+// Functions
+//////////////////////////////////////////////////////////////////////////
+
+// Constructor
+EmonLibPro::EmonLibPro(void) { }
+
+
+// Call this one time
+void EmonLibPro::begin()
+{
+     // If a lower resolution than 10 bits is needed, the input clock frequency to the ADC can be higher than 200kHz to get a higher sample rate.
+     ADCSRA &= 0b11111000;     // remove bits set by Arduino library
+     //ADCSRA |= 0b00000110;   // change ADC prescaler to /64 = 250kHz clock (less precision)
+     ADCSRA |= 0b00000111;     // change ADC prescaler to /128 = 125kHz clock
+     DIDR0   = 0b00111111;     // ADC5D...ADC0D: Digital Input Disable, saves power
+     
+     FlagOutOfTime = false;
+     FlagPllUpdated = false;
+     FlagCALC_READY = false;
+     FlagINVALID_DATA = true;
+     pllUnlocked = 0xFF;       //initial number of correct cycles to unlock PLL
+     
+     //set timer 1 interrupt for required period
+     cli();
+     TCCR1A = 0; // clear control registers
+     TCCR1B = 0;
+     TCNT1  = 0; // clear counter
+     OCR1A  = TIMERTOP;      // set compare reg for timer period
+
+     TCCR1B |= (1UL<<WGM12);   // CTC mode
+     TCCR1B |= (1UL<<CS10);    // clkIO/1 (No prescaling)
+     TIMSK1 |= (1UL<<OCIE1A);  // enable timer 1 compare interrupt
+     ADCSRA |= (1UL<<ADIE);    // enable ADC interrupt
+     sei(); //Enable interrupts
+}
+
+
+// calculate voltage, current, power and frequency
+void EmonLibPro::calculateResult()
+{
+  if(SamplesPerCycle!=0) {
+        // Process accumulated data as follows:
+        //
+        // Active power:	P = (accumulated data) / (number of samples)
+        // Voltage:		U = SQRT [ (accumulated data) / (number of samples) ]
+        // Current:		I = SQRT [ (accumulated data) / (number of samples) ]
+        // Apparent power:	S = U * I
+        //
+  
+        volatile byte i;
+        for (i=0;i<VOLTSCOUNT;i++){
+		        ResultV[i].U = CalCoeff.VRATIO[i] * (sqrt((float)TotalV[i].U2/SamplesPerCycleTotal)); //Vrms
+		        //ResultV[i].HZ = SAMPLESPSEC * (float)SamplesPerTotal / (float)SamplesPerCycleTotal ;
+                ResultV[i].HZ = TotalV[i].cHZ / (float)CyclesPerTotal / 100;
+		        TotalV[i].U2=0;
+                TotalV[i].cHZ=0;
+        }
+
+        for (i=0;i<CURRENTCOUNT;i++){
+		        //I
+		        ResultP[i].I = CalCoeff.IRATIO[i] * (sqrt((float)TotalP[i].I2/SamplesPerCycleTotal)); // Irms
+		        //P
+                #if CURRENTCOUNT == VOLTSCOUNT
+		        ResultP[i].P = (CalCoeff.IRATIO[i] * CalCoeff.VRATIO[i]) * ((float)TotalP[i].P/SamplesPerCycleTotal); // Active power (RealPower)
+                ResultP[i].S = ResultV[i].U * ResultP[i].I; // Apparent power
+                #else
+		        ResultP[i].P = (CalCoeff.IRATIO[i] * CalCoeff.VRATIO[0]) * ((float)TotalP[i].P/SamplesPerCycleTotal); // Active power (RealPower)
+		        ResultP[i].S = ResultV[0].U * ResultP[i].I; // Apparent power
+                #endif
+                ResultP[i].F = absolute((float)ResultP[i].P / ResultP[i].S);
+		        TotalP[i].I2 = 0;
+		        TotalP[i].P = 0;
+        }
+        SamplesPerCycleTotal=0;
+        CyclesPerTotal=0;
+
+    
+  } else {
+        EmonLibPro::FlagINVALID_DATA = true;
+        // Clean results, we have invalid data.
+        volatile byte i;
+        for (i=0;i<VOLTSCOUNT;i++){
+            EmonLibPro::CycleV[i].U2 = 0;
+            EmonLibPro::CycleV[i].cHZ = 0;
+            EmonLibPro::ResultV[i].U = 0;
+            EmonLibPro::ResultV[i].HZ = 0;
+        }
+        for (i=0;i<CURRENTCOUNT;i++){
+            EmonLibPro::CycleP[i].I2 = 0;
+            EmonLibPro::CycleP[i].P = 0;
+            EmonLibPro::ResultP[i].I = 0;
+            EmonLibPro::ResultP[i].P = 0;
+            EmonLibPro::ResultP[i].S = 0;
+            EmonLibPro::ResultP[i].F = 0;
+        }
+  }
+  FlagCALC_READY=false;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// INTERRUPT Routines
+//////////////////////////////////////////////////////////////////////////
+
+// Timer 1 interrupt handler
+ISR(TIMER1_COMPA_vect) {
+    ADMUX   = _BV(REFS0) | 0; // [AVCC with external capacitor at AREF pin] | [PIN=ADC0]
+    ADCSRA |= _BV(ADSC);      // Start ADC conversion
+    EmonLibPro::AdcId=0;
+    if (EmonLibPro::FlagOutOfTime) {
+        //Serial.println("$");  // Alarm that previous ADC processing has run out of time before timer event. Reduce sampling rate!
+    }
+    if (EmonLibPro::SamplePerAcc > 250 && !EmonLibPro::FlagINVALID_DATA) { // Clean data when no zero cross detected.
+            EmonLibPro::SamplesPerCycle=0;
+            EmonLibPro::FlagCALC_READY = true;
+    }
+}
+
+
+// ADC interrupt handler
+ISR(ADC_vect) {
+    volatile unsigned long timerVal = TCNT1;    // Saves timer counter for freq measurement
+    volatile signed long  TempI;
+    volatile signed long TempL;
+    volatile byte i;
+    
+    // Maintain multiplexing of input channels
+    if (EmonLibPro::AdcId < VOLTSCOUNT + CURRENTCOUNT - 1) {
+        ADMUX = _BV(REFS0) | (EmonLibPro::AdcId + 1);   // Select next ADC
+        ADCSRA |= _BV(ADSC);                            // Start conversion
+    } 
+
+    // ## 1 - Saves Previous read
+    // x[n+1] <- x[n]
+    EmonLibPro::Sample[EmonLibPro::AdcId].Previous=EmonLibPro::Sample[EmonLibPro::AdcId].Fresh;
+    EmonLibPro::Sample[EmonLibPro::AdcId].Fresh=ADC;
+
+
+    // ## 2 - Apply filter for DC offset removal. Must use long data types for precision!
+    // y[n] <- y[n-1]
+    EmonLibPro::Sample[EmonLibPro::AdcId].PreviousFiltered=EmonLibPro::Sample[EmonLibPro::AdcId].Filtered;
+    
+    
+    //	y[n] = 0.996*y[n-1] + 0.996*x[n] - 0.996*x[n-1]
+    
+    EmonLibPro::Sample[EmonLibPro::AdcId].Filtered = 
+                0.996 * (EmonLibPro::Sample[EmonLibPro::AdcId].PreviousFiltered + 
+                         (EmonLibPro::Sample[EmonLibPro::AdcId].Fresh-EmonLibPro::Sample[EmonLibPro::AdcId].Previous)
+                        );
+    
+    //EmonLibPro::Sample[EmonLibPro::AdcId].Filtered = EmonLibPro::Sample[EmonLibPro::AdcId].Fresh - 512;
+    
+    // Algorithm 1
+    /*
+    TempL=((long)EmonLibPro::Sample[EmonLibPro::AdcId].PreviousFiltered<<8)-EmonLibPro::Sample[EmonLibPro::AdcId].PreviousFiltered;
+    TempL=TempL>>8;
+    TempI=EmonLibPro::Sample[EmonLibPro::AdcId].Fresh-EmonLibPro::Sample[EmonLibPro::AdcId].Previous;
+    TempL=TempL + (((long)TempI<<8)-(long)TempI);
+    EmonLibPro::Sample[EmonLibPro::AdcId].Filtered=TempL;
+    */
+
+    // Algorithm ori
+    /*
+    TempL=255*(long)EmonLibPro::Sample[EmonLibPro::AdcId].Filtered;
+    TempL=TempL>>8;
+    TempI=EmonLibPro::Sample[EmonLibPro::AdcId].Fresh-EmonLibPro::Sample[EmonLibPro::AdcId].Previous;
+    TempL=TempL + 255*(long)TempI;
+        TempL=TempL>>6;
+    EmonLibPro::Sample[EmonLibPro::AdcId].Filtered=TempL;
+    */
+    
+    // Algorithm 2
+/*
+    TempI=EmonLibPro::Sample[EmonLibPro::AdcId].Fresh - EmonLibPro::Sample[EmonLibPro::AdcId].Previous;  // find the input change
+    TempL= (long)TempI<<8;                                                 // rescale the input change (x256)
+    TempL=TempL + EmonLibPro::Sample[EmonLibPro::AdcId].PreviousFiltered;  // add the previous o/p
+    EmonLibPro::Sample[EmonLibPro::AdcId].Filtered=TempL-((long)TempL>>8); // subtract 1/256, same as x255/256
+*/
+
+
+    // ## 3 - Apply phase calibration. Due to multiplexing, there will be a delay
+    // between subsequent data channels, as follows:
+    //
+    // 	Delay = [ 360 degrees * f(mains) ] / [ 3 * f(sampling) ]
+    //
+    // At 2400Hz sampling rate, this means that fresh samples from ADC0
+    // and ADC1, for example, will actually have a time difference of
+    // 1/2400 = 0.42ms, which corresponds to (50*360)/2400 = 7.5 degrees
+    // in a 50Hz mains environment. In addition, current transformers in
+    // the analogue front end may typically contribute up to six degrees
+    // of additional phase lag. Phase distortions are calibrated using
+    // linear interpolation, as follows:
+    //
+    //	Calibrated = x[n-1] + CalibCoefficient * ( x[n] - x[n-1] )
+    //
+    // Note that linear interpolation is the same as a fixed time delay.
+    // This means that higher frequency components will not be adjusted
+    // correctly. Typically, though, not enough energy is present in the
+    // higher frequency components to cause this to degrade accuracy below
+    // acceptable levels.
+    //
+    // Also note that the higher the sampling rate, the lower is the
+    // calibration range in degrees. If the sampling rate is too high,
+    // then the linear interpolation as such can not compensate for the
+    // phase lag of some DC imuned current transformers. Such
+    // transformers can have a phase lag in excess of six degrees.
+    TempL=EmonLibPro::Sample[EmonLibPro::AdcId].Filtered-EmonLibPro::Sample[EmonLibPro::AdcId].PreviousFiltered;
+    TempL=TempL*CalCoeff.PCC[EmonLibPro::AdcId];
+    TempL=TempL>>16;
+    EmonLibPro::Sample[EmonLibPro::AdcId].PreviousCalibrated = EmonLibPro::Sample[EmonLibPro::AdcId].Calibrated;
+    EmonLibPro::Sample[EmonLibPro::AdcId].Calibrated=EmonLibPro::Sample[EmonLibPro::AdcId].Filtered-TempL;
+    
+    //phaseShiftedV=lastV+((((long)newV-lastV)*I1PHASESHIFT)>>8);
+
+    // ## 4 - Perform Zero cross check
+    if (!EmonLibPro::Sample[EmonLibPro::AdcId].WaitNextCross) {
+        EmonLibPro::Sample[EmonLibPro::AdcId].WaitNextCross=EmonLibPro::Sample[EmonLibPro::AdcId].Calibrated < -32; // Only allow next zero cross if already passed this value on the cycle
+    } else {
+        //Is Positive ?
+        if (EmonLibPro::Sample[EmonLibPro::AdcId].Calibrated >= 0) {
+            //Is Rising?
+            if (EmonLibPro::Sample[EmonLibPro::AdcId].Calibrated >= EmonLibPro::Sample[EmonLibPro::AdcId].PreviousCalibrated) {
+                EmonLibPro::Sample[EmonLibPro::AdcId].PreviousTimerVal = EmonLibPro::Sample[EmonLibPro::AdcId].TimerVal;
+                EmonLibPro::Sample[EmonLibPro::AdcId].TimerVal = timerVal;
+                EmonLibPro::Sample[EmonLibPro::AdcId].FlagZeroDetec=true;
+                EmonLibPro::Sample[EmonLibPro::AdcId].WaitNextCross=false;
+            }
+        }
+    }
+
+
+    // Last sensor reading
+    if (EmonLibPro::AdcId == VOLTSCOUNT + CURRENTCOUNT - 1)
+    {
+        //Serial.println(EmonLibPro::Sample[0].Calibrated);
+
+        // Quatrant 0, Cycle start. Only for Voltage sensor 0 this time.
+        if (EmonLibPro::Sample[0].FlagZeroDetec) {
+            // One New CYCLE Starts
+            EmonLibPro::Sample[0].FlagZeroDetec=false;
+            EmonLibPro::FlagCYCLE_FULL=true;
+
+            EmonLibPro::SamplesPerCycle=EmonLibPro::SamplePerAcc / (VOLTSCOUNT + CURRENTCOUNT) ;    //Samples per cycle per sensor
+            EmonLibPro::SamplePerAcc = 0;
+            
+            // ### PLL processing ###
+            // The idea is to adjust timer to get a close to 0VAC read on the first measure of the next cycle.
+            // Without PLL max resolution is 1/SAMPLESPSEC (0.83 ms for 1200samples/s)
+            
+            TCNT1 = TCNT1 + ((unsigned int)EmonLibPro::Sample[0].Calibrated * (double)(PLLTIMERDELAYCOEF));
+            if((unsigned int)(EmonLibPro::Sample[0].Calibrated) > (512/EmonLibPro::SamplesPerCycle)) { //sample > min temporal resolution /2
+                EmonLibPro::FlagPllUpdated=true;
+                EmonLibPro::pllUnlocked=EmonLibPro::SamplesPerCycle; // we're unlocked
+            } else {
+                if(EmonLibPro::pllUnlocked) {
+                    EmonLibPro::pllUnlocked--;
+                }
+            }
+            
+            #ifdef LEDISLOCK
+            //digitalWrite(LEDPIN,pllUnlocked?LOW:HIGH);
+            #endif
+
+            // When a full cycle of data has been accumulated; make a copy of accumulated
+            // data before it is overwritten by next sampling instance.
+            for (i=0;i<VOLTSCOUNT;i++){
+                EmonLibPro::CycleV[i].U2 = EmonLibPro::AccumulatorV[i].U2;
+                EmonLibPro::TotalV[i].U2 +=EmonLibPro::AccumulatorV[i].U2;
+                //Freq in centyHZ (Hz*100)
+                EmonLibPro::CycleV[i].cHZ = (unsigned long)(F_CPU*100) / 
+                                            (
+                                               ( (TIMERTOP * (uint8_t)(EmonLibPro::SamplesPerCycle))
+                                                 + (unsigned int) EmonLibPro::Sample[i].PreviousTimerVal
+                                               ) 
+                                               - (unsigned int)EmonLibPro::Sample[i].TimerVal  
+                                            ) ;
+                EmonLibPro::TotalV[i].cHZ+=EmonLibPro::CycleV[i].cHZ;
+                EmonLibPro::AccumulatorV[i].U2 = 0;
+            }
+            for (i=0;i<CURRENTCOUNT;i++){
+                EmonLibPro::CycleP[i].I2 = EmonLibPro::AccumulatorP[i].I2;
+                EmonLibPro::TotalP[i].I2 +=EmonLibPro::AccumulatorP[i].I2;
+                EmonLibPro::CycleP[i].P = EmonLibPro::AccumulatorP[i].P;
+                EmonLibPro::TotalP[i].P +=EmonLibPro::AccumulatorP[i].P;
+                EmonLibPro::AccumulatorP[i].I2 = 0;
+                EmonLibPro::AccumulatorP[i].P = 0;
+            }
+            
+            EmonLibPro::SamplesPerCycleTotal+=(EmonLibPro::SamplesPerCycle);
+            EmonLibPro::CyclesPerTotal++;          // for average frequency calculation
+            //EmonLibPro::FlagCYCLE_FULL=false;
+            if (EmonLibPro::CyclesPerTotal >= CALCULATESAMPLES) {
+                EmonLibPro::FlagCALC_READY = true;
+                EmonLibPro::FlagINVALID_DATA = false;
+            }
+
+        }
+
+    }
+    
+
+    // Save measurement data for voltage, current and active power on all
+    // channels. For active power measurements; accumulate instantaneous
+    // power product. For current and voltage measurements: accumulate
+    // square of samples
+    //
+    // Range check:
+    //
+    // 	Filtered data:		Filtered  Range=[FFFE0280...0001FD80]
+    // 	Prescaled data (>>5):	TempX	  Range=[FFFFF014...00000FEC]
+    // 	Multiplication result:	(n/a)	  Range=[FF027E70...00FD8190]
+    //
+    // 	Prescaled data (>>6):	TempX	  Range=[FFFFF80A...000007F6]
+    // 	Multiplication result:	(n/a)	  Range=[FFC09F9C...003F6064]
+    //
+    // Assuming that sampled data is stuck at full-scale (which it can't
+    // be, since the high-pass filter would bias such a signal to zero),
+    // then for accumulation purposes the maximum number of samples that
+    // can be integrated is:
+    //
+    // 	MaxSamples(presc=32) = 7FFFFFFFh / 00FD8190h = 0081h = 129d
+    // 	MaxSamples(presc=64) = 7FFFFFFFh / 003F6064h = 0205h = 517d
+    //
+    // Note: Index=0 -> ADC0, Index=1 -> ADC1, Index=2 -> ADC2
+    EmonLibPro::Temp[EmonLibPro::AdcId]=EmonLibPro::Sample[EmonLibPro::AdcId].Calibrated;//>>6;
+
+    if (EmonLibPro::AdcId < VOLTSCOUNT)
+    {
+        EmonLibPro::AccumulatorV[EmonLibPro::AdcId].U2 += (EmonLibPro::Temp[EmonLibPro::AdcId]*EmonLibPro::Temp[EmonLibPro::AdcId]);
+    }
+    else
+    {
+        EmonLibPro::AccumulatorP[EmonLibPro::AdcId-VOLTSCOUNT].I2 += (EmonLibPro::Temp[EmonLibPro::AdcId]*EmonLibPro::Temp[EmonLibPro::AdcId]);
+        #if CURRENTCOUNT == VOLTSCOUNT
+        EmonLibPro::AccumulatorP[EmonLibPro::AdcId-VOLTSCOUNT].P += EmonLibPro::Temp[EmonLibPro::AdcId - CURRENTCOUNT]*EmonLibPro::Temp[EmonLibPro::AdcId];
+        #else
+        EmonLibPro::AccumulatorP[EmonLibPro::AdcId-VOLTSCOUNT].P += EmonLibPro::Temp[VOLTSCOUNT-1]*EmonLibPro::Temp[EmonLibPro::AdcId]; //V0 * I[AdcId]
+        #endif
+    
+    }
+
+    EmonLibPro::SamplePerAcc++;	        // Sample sensor data counter increment
+    EmonLibPro::AdcId++;                // ADC index increment
+    EmonLibPro::FlagOutOfTime = (TIFR1 & (1 << OCF1A));  // If timer counter is set we already missed a timer event. Signal this.
+}
